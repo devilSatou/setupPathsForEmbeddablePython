2d124a9b11be0959c5fce752afffb07f65537ffd
devilSatou <151437156+devilSatou@users.noreply.github.com>
2024-11-21 04:38:53 +0000

    '.'をos.getcwd()に修正

--- setuppaths/setuppaths/setuppaths.py
+++ setuppaths/setuppaths/setuppaths.py
@@ -3,9 +3,9 @@ import sys
 
 def add_project_paths():
     # カレントディレクトリを追加
-    sys.path.append(os.path.abspath('.'))
+    sys.path.append(os.path.abspath(os.getcwd()))
 
     # サブディレクトリを再帰的に追加
-    for root, dirs, files in os.walk(os.path.abspath('.')):
+    for root, dirs, files in os.walk(os.path.abspath(os.getcwd())):
         for dir in dirs:
             sys.path.append(os.path.join(root, dir))
